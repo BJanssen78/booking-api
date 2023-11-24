0ef40f34d59a2b8f888cafb919e6d0de97fb7212
Ben Janssen <98946596+BJanssen78@users.noreply.github.com>
2023-11-24 21:40:28 +0100

    deleted consoles

--- services/bookings/getAllBookings.js
+++ services/bookings/getAllBookings.js
@@ -3,15 +3,11 @@ import { PrismaClient } from "@prisma/client";
 const getAllBookings = async (
   userId,
   bookingStatus,
-  // checkinDate,
   checkinDateWithoutTime,
   checkoutDate,
   propertyId
 ) => {
   const prisma = new PrismaClient();
-  // let checkinDate = checkinDateWithoutTime + "T23:59:59.999Z";
-  console.log(checkinDateWithoutTime);
-  // console.log(checkinDate);
 
   return prisma.booking.findMany({
     where: {
